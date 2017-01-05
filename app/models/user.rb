bba92fc90a3bf4ca33d2cf3fc55d1edae4558c92
FrankieMFerreira <FrankieMFerreira@gmail.com>
2017-01-05 16:08:40 -0500

    Okay.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+
+
+  def mood
+    if nausea > happiness
+      "sad"
+    elsif happiness > nausea
+      "happy"
+    else
+      "neutral"
+    end
+
+  end
 end
